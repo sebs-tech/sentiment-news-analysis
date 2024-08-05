6fc18669598e1ec239c22cb47997dcc4cc62d1e8
sebs.tech <sebastjan.rijavec@gmail.com>
2024-08-05 18:39:36 +0200

    changed sentiment analyiss to focus on description

--- src/components/google_sentiment_analysis.py
+++ src/components/google_sentiment_analysis.py
@@ -11,7 +11,7 @@ class GoogleSentimentAnalysis:
         self.tokenizer = AutoTokenizer.from_pretrained(self.config.get_nlp_tokenizer_path())
         self.model = AutoModelForSequenceClassification.from_pretrained(self.config.get_nlp_model_path())
         
-        #self.perform_sentiment_analysis()
+        self.perform_sentiment_analysis()
 
 
     def get_sentiment_analysis(self, tokenization):
@@ -23,6 +23,6 @@ class GoogleSentimentAnalysis:
     def perform_sentiment_analysis(self):
         # Read data frame 
         df = pd.read_csv(self.config.raw_news_storage_path)
-        df['sentiment'] = df['title'].apply(self.get_sentiment_analysis)
+        df['sentiment'] = df['description'].apply(self.get_sentiment_analysis)
         os.makedirs(os.path.dirname(self.config.get_sentiment_news_storage_path()), exist_ok=True)
         df.to_csv(self.config.get_sentiment_news_storage_path(), index=False, header=True)
